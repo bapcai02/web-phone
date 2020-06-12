4dcf36bf81e2dc0baac28e64f0167c2082b4a488
bapcai02 <44980082+bapcai02@users.noreply.github.com>
2020-06-12 08:45:28 +0700

    put
    put

---
+++ database/migrations/2020_06_10_090123_create_bill_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bill', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_customer');
+            $table->time('date_order');
+            $table->integer('total');
+            $table->string('note');
+            $table->string('payment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bill');
+    }
+}
